c4d1252c225d8c242b31ba375e90adcd7c1a2c86
Samuel Llibre-Pillco <dev@zyruks.com>
2024-04-28 20:13:20 -0300

    feat(simple-card): ✨ add SimpleCard component with optional inversion and border
    Introduces SimpleCard, leveraging CardWrapper for consistent styling. Supports optional inverted
    layout, external navigation via URL, and dynamic text and image content. Default settings include
    primary variant and bordered style.

---
+++ src/components/Cards/Cards.type.ts
@@ -0,0 +1,5 @@
+export enum CardVariant {
+  primary = 'primary',
+  secondary = 'secondary',
+  tertiary = 'tertiary',
+}
--- src/components/Cards/index.ts
+++ src/components/Cards/index.ts
@@ -1,1 +1,3 @@
+export * from './Cards.type';
 export * from './CardWrapper';
+export { default as SimpleCard } from './SimpleCard/SimpleCard.astro';
