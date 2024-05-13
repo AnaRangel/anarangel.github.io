2082eaaf4f2c3ca15b5890237699882616fa2e6e
Miguel Colmenarez <m.colmenarez.dev@gmail.com>
2024-05-13 18:40:43 -0400

    feat: index.ts change cause removing the navbar folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { default as Button } from './Button/Button.astro';
 export * from './Cards';
 export { default as Footer } from './Footer.astro';
 export * from './Icon';
-export { default as Navbar } from './Navbar/Navbar.astro';
+export { default as Navbar } from './Navbar.astro';
