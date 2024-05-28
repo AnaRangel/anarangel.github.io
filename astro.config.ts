988d3f49f549f836cc9e3298c9b8bb26693b7ac4
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-28 19:05:02 -0400

    chore: update site url on astro config

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: Change url to the website
-  site: 'https://www.example.com',
+  site: 'https://anarangel.github.io',
   vite: {
     build: {
       cssMinify: 'lightningcss',
