a641a7337546253556dc06f46498708f102ce8a6
Felix Icaza <fx.joliett17@gmail.com>
2024-06-06 23:04:15 -0600

    chore: add external domains for designers contributos in astro config

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anarangel.github.io',
+  image: {
+    domains: ['media.licdn.com'],
+  },
   vite: {
     build: {
       cssMinify: 'lightningcss',
